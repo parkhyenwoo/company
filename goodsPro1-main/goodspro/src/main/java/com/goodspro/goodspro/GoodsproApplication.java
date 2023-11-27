816e57951f8802df5a1f9109cb2e4db272b095d4
parkhyenwoo <113586323+parkhyenwoo@users.noreply.github.com>
2023-11-27 21:49:01 +0900

    hyenwooProject
    hyenwooProject

---
+++ goodsPro1-main/goodspro/src/main/java/com/goodspro/goodspro/GoodsproApplication.java
@@ -0,0 +1,13 @@
+package com.goodspro.goodspro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodsproApplication.class, args);
+	}
+
+}
---
+++ goodsPro1-main/goodspro/src/test/java/com/goodspro/goodspro/GoodsproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodspro.goodspro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
